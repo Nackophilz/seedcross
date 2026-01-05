784c674fcfe30551ff6c8e0684801fb31a12a67d
ElPouki <61667226+Nackophilz@users.noreply.github.com>
2026-01-05 21:22:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-#cd /code/seedcross
+
+# On s'assure d'être dans le bon dossier
+cd /app
+
+# 1. Appliquer les migrations
+echo "Application des migrations..."
+python manage.py migrate --noinput
+
+# 2. Collecter les fichiers statiques
+# python manage.py collectstatic --noinput
+
+# 3. Lancer les tâches de fond
+echo "Lancement des tâches de fond..."
 python manage.py process_tasks &
+
+# 4. Lancer le serveur
+echo "Démarrage du serveur..."
 python manage.py runserver 0.0.0.0:8019
